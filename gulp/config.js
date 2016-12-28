fc0668dc237ae44ed01cccdc78d24dda3ce3e3a8
Francisco Vena <fvena32@gmail.com>
2016-12-28 14:32:50 +0100

    add gulp

---
+++ gulp/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  paths: {
+    tmp :  './.tmp',
+    css:   './.tmp/css/*.css',
+    cssFolder: './.tmp/css',
+    html:  './.tmp/*.html',
+    src :  './app',
+    js:    './app/scripts/**/*.js',
+    pug:   './app/**/*.pug',
+    scss:  './app/stylesheets/**/*.scss',
+    fonts: './app/assets/fonts',
+    img:   './app/assets/images',
+    dist:  './dist'
+  }
+}
---
+++ gulpfile.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var gulp        = require('gulp'),
+    requireDir  = require('require-dir')('./gulp/tasks', { recurse: true });
